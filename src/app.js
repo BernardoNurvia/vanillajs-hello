b5603acd53ddbdb81ab9312609103b973c496585
BernardoNurvia <167910567+BernardoNurvia@users.noreply.github.com>
2024-05-25 11:21:33 +0000

    First commit genExcuse

--- src/app.js
+++ src/app.js
@@ -8,4 +8,40 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = listElement => {
+    let element = "";
+    element = listElement[Math.floor(Math.random() * listElement.length)];
+    return element;
+  };
+  console.log(
+    getRandomElement(who) +
+      " " +
+      getRandomElement(action) +
+      " " +
+      getRandomElement(what) +
+      " " +
+      getRandomElement(when)
+  );
+  let excuseGen = document.querySelector("#excuse");
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
+
+  console.log("excuseGen");
 };
